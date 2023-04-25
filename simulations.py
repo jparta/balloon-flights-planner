ba9801d8c4935bbca4c9379d7ca7b3b5f223a978
Joonatan Partanen <joonatan.partanen@iceye.fi>
2023-04-25 16:44:58 +0300

    Get a simulation going

---
+++ simulations.py
@@ -0,0 +1,44 @@
+import logging
+from datetime import datetime, timedelta
+from pathlib import Path
+
+import numpy as np
+from astra.simulator import flight, forecastEnvironment
+
+
+logging.basicConfig(level=logging.DEBUG)
+
+np.random.seed(42)
+
+output_path = Path("astra_out")
+time_diff_days = 1
+
+LAUNCH = {
+    "launchSiteLat": 60.157502,
+    "launchSiteLon": 24.654051,
+    "launchSiteElev": 40,
+    "launchTime": datetime.now() + timedelta(days=1),
+    "inflationTemperature": 10,
+    "forceNonHD": False
+}
+
+FLIGHT_SIM = {
+    "balloonGasType": "gt96He",
+    "balloonModel": "SFB800",
+    "nozzleLift": 1.6,
+    "payloadTrainWeight": 0.664,
+    "parachuteModel": "",
+    "numberOfSimRuns": 10,
+    "trainEquivSphereDiam": 0.285,  # Styrox probe cuboid area-equivalent diameter
+    "outputPath": output_path.absolute(),
+    "debugging": True,
+    "log_to_file": True,
+}
+
+def run_sim():
+    sim_environment = forecastEnvironment(**LAUNCH)
+    the_flight = flight(**FLIGHT_SIM, environment=sim_environment)
+    the_flight.run()
+
+if __name__ == "__main__":
+    run_sim()

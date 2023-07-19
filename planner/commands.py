7c982d8ec5b2bb2c4cd99f5f82ff5bb885a091f6
Joonatan Partanen <joonatan.partanen@iceye.fi>
2023-07-19 16:07:40 +0300

    Add db wipe command, rename

--- planner/commands.py
+++ planner/commands.py
@@ -4,10 +4,18 @@ from flask import Blueprint
 
 utils_bp = Blueprint('utils', __name__)
 
-@utils_bp.cli.command('initdb')
-def initdb():
+@utils_bp.cli.command('init_db')
+def init_db():
     """Initialize the database."""
-    click.echo('Init the db')
+    click.echo('Initializing the db')
     from planner import models, db
     db.create_all()
+    click.echo('Done')
+
+@utils_bp.cli.command('wipe_db')
+def wipe_db():
+    """Drop everything."""
+    click.echo('Wiping the db')
+    from planner import models, db
+    db.drop_all()
     click.echo('Done')

45e30999bd1f4da8f410dd92ceb273e8d6cd639d
Joonatan Partanen <joonatan.partanen@iceye.fi>
2023-08-21 11:52:02 +0300

    Show new launch time on tooltip, simplify view

--- planner/main/routes.py
+++ planner/main/routes.py
@@ -1,9 +1,12 @@
+from datetime import datetime, timezone
+from pprint import pformat
+import time
 import folium
 import geopandas as gpd
 import pandas as pd
 from flask import render_template, current_app
-from folium import plugins, map as fol_map
-from sqlalchemy import func
+from folium import features
+from sqlalchemy import and_, func
 
 from planner import db
 from planner.main import bp
@@ -37,51 +40,97 @@ def index():
     m.get_root().width = "100%"
     m.get_root().height = "100%"
 
-    # query all the TrajectoryPredictionData objects from the database
     kdes = []
     bad_landing_areas = []
     landing_points = []
-    latest_run = db.session.query(func.max(TrajectoryPredictionData.run_at))
-    for prediction_data in db.session.query(TrajectoryPredictionData).filter(TrajectoryPredictionData.run_at == latest_run).all():
+
+    query_start = time.time()
+    # query the latest trajectory predictions for each launch time
+    subq = db.session.query(
+        TrajectoryPredictionData.launch_time,
+        func.max(TrajectoryPredictionData.run_at).label("max_run_at")
+    ).group_by(TrajectoryPredictionData.launch_time).subquery()
+    query = db.session.query(TrajectoryPredictionData).join(
+        subq,
+        and_(
+            TrajectoryPredictionData.launch_time == subq.c.launch_time,
+            TrajectoryPredictionData.run_at == subq.c.max_run_at
+        )
+    ).filter(TrajectoryPredictionData.launch_time >= datetime.now(timezone.utc))
+
+    # load to GeoPandas
+    # gpd.read_postgis(query.statement, query.session.get_bind(), geom_col='kde')
+
+    """
+    for prediction_data in query.all():
         gs = prediction_data.to_GeoSeries()
         kdes.append(gs["kde"])
         bad_landing_areas.append(gs["bad_landing_areas"])
         landing_points.append(gs["landing_points"])
+    query_end = time.time()
+    current_app.logger.info(f"Query took {query_end - query_start} seconds")
 
-    landing_layer = fol_map.FeatureGroup(name="Predicted landing locations")
-    landing_xys = join_list_of_geoseries(landing_points).to_crs("4326").get_coordinates()
-    for loc in zip(landing_xys['y'], landing_xys['x']):
-        folium.CircleMarker(loc, radius=1, color="black", fill=True).add_to(landing_layer)
-    landing_layer.add_to(m)
-    # plugins.MarkerCluster(landing_xys[['y', 'x']], name="Predicted landing locations").add_to(m)
-
-    folium.Choropleth(
-        name="Kernel density estimate",
-        geo_data=join_list_of_geoseries(kdes),
-        fill_color="yellow",
-        fill_opacity=0.5,
-        line_color="black",
-        line_weight=1,
-        highlight=False,
-    ).add_to(m)
-
-    folium.Choropleth(
-        name="Bad landing areas",
-        geo_data=join_list_of_geoseries(bad_landing_areas),
-        fill_color="red",
-        highlight=False,
-    ).add_to(m)
-    
+    # compare size of kde and bad landing areas in bytes
+    kde_size = sum([kde.to_json().encode('utf-8').__sizeof__() for kde in kdes])
+    current_app.logger.info(f"KDE size: \n{kde_size:>20} bytes")
+    bad_landing_size = sum([bad_landing.to_json().encode('utf-8').__sizeof__() for bad_landing in bad_landing_areas if bad_landing is not None])
+    current_app.logger.info(f"Bad landing size: \n{bad_landing_size:>20} bytes")
     """
-    for layer_name, layer_geometry in layer_data.items():
-        geojson = mapready_geojson_from_geoseries(layer_geometry, layer_name)
-        if geojson is not None:
-            folium.GeoJson(geojson, name=layer_name).add_to(m)
-    """
+
+    layer_add_start = time.time()
+    for prediction_data in query.all():
+        prediction_df = prediction_data.to_gdf()
+        prediction_df["launch_time"] = prediction_df["launch_time"].map(lambda dt: dt.isoformat())
+
+        # current_app.logger.info(f"Prediction data: \n{prediction_df.dtypes}")
+        kde_json = prediction_df[["launch_time", "kde"]].set_geometry("kde").to_json(to_wgs84=True)
+        # current_app.logger.info(f"KDE json: \n{pformat(kde_json)}")
+        kde_layer = folium.Choropleth(
+            name="Kernel density estimate",
+            geo_data=kde_json,
+            fill_color="yellow",
+            fill_opacity=0.5,
+            line_color="black",
+            line_weight=1,
+            highlight=False,
+        )
+        kde_layer.geojson.add_child(features.GeoJsonTooltip(["launch_time"]))
+        kde_layer.add_to(m)
+
+        """
+        landing_layer = fol_map.FeatureGroup(name="Predicted landing locations")
+        landing_xys = join_list_of_geoseries(landing_points).to_crs("4326").get_coordinates()
+        for loc in zip(landing_xys['y'], landing_xys['x']):
+            folium.CircleMarker(loc, radius=0.5, color="black", fill=True).add_to(landing_layer)
+        landing_layer.add_to(m)
+
+        folium.Choropleth(
+            name="Bad landing areas",
+            geo_data=join_list_of_geoseries(bad_landing_areas),
+            fill_color="red",
+            highlight=False,
+        ).add_to(m)
+        """
+    layer_add_end = time.time()
+    current_app.logger.info(f"Layer add took {layer_add_end - layer_add_start} seconds")
 
     folium.LayerControl(collapsed=False).add_to(m)
-
+    
     m.fit_bounds(m.get_bounds(), padding=(30, 30))
 
+    getting_iframe_start = time.time()
     iframe = m.get_root()._repr_html_()
-    return render_template('index.html', iframe=iframe)
+    getting_iframe_end = time.time()
+    current_app.logger.info(f"Getting iframe took {getting_iframe_end - getting_iframe_start} seconds")
+
+    template_render_start = time.time()
+    resp_content = render_template('index.html', iframe=iframe)
+    template_render_end = time.time()
+    current_app.logger.info(f"Template render took {template_render_end - template_render_start} seconds")
+    return resp_content
+
+
+
+@bp.route('/sites')
+def sites():
+    pass
--- planner/models.py
+++ planner/models.py
@@ -1,5 +1,6 @@
 import geopandas as gpd
 from geoalchemy2 import Geometry
+from sqlalchemy.ext.hybrid import hybrid_property
 
 from planner.extensions import db
 
@@ -8,22 +9,51 @@ class TrajectoryPredictionData(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     run_at = db.Column(db.DateTime, nullable=False)
     launch_time = db.Column(db.DateTime, nullable=False)
-    landing_points = db.Column(Geometry(geometry_type='MULTIPOINT'), nullable=False)
+    landing_points = db.Column(Geometry(geometry_type='MULTIPOINT'), nullable=False, )
     kde = db.Column(Geometry(geometry_type='MULTIPOLYGON'), nullable=False)
     bad_landing_areas = db.Column(Geometry(geometry_type='MULTIPOLYGON'), nullable=True)
     bad_landing_proportion = db.Column(db.Float, nullable=False)
 
+    @staticmethod
+    def geoalchemy_to_geoseries(obj):
+        if obj is None:
+            return None
+        crs = obj.srid
+        geoseries = gpd.GeoSeries.from_wkb([str(obj.as_ewkb())])
+        geoseries = geoseries.set_crs(crs)
+        return geoseries
+
     def __repr__(self):
         return f'<TrajectoryPredictionData {self.id}>'
+    
+    """
+    @hybrid_property
+    def landing_points(self):
+        return self.geoalchemy_to_geoseries(self._landing_points)
+    
+    @hybrid_property
+    def kde(self):
+        return self.geoalchemy_to_geoseries(self._kde)
+    
+    @hybrid_property
+    def bad_landing_areas(self):
+        return self.geoalchemy_to_geoseries(self._bad_landing_areas)
+    """
 
-    def to_GeoSeries(self):
-        fields_to_output = {
+    def to_gdf(self):
+        geom_fields_to_output = {
             "landing_points": self.landing_points,
             "kde": self.kde,
             "bad_landing_areas": self.bad_landing_areas,
         }
+        other_fields_to_output = {
+            "id": self.id,
+            "run_at": self.run_at,
+            "launch_time": self.launch_time,
+            "bad_landing_proportion": self.bad_landing_proportion,
+        }
         output = {}
-        for name, geoset in fields_to_output.items():
+        for name, geoset in geom_fields_to_output.items():
             if geoset is None:
                 output[name] = None
                 continue
@@ -31,4 +61,5 @@ class TrajectoryPredictionData(db.Model):
             geoseries = gpd.GeoSeries.from_wkb([str(geoset.as_ewkb())])
             geoseries = geoseries.set_crs(crs)
             output[name] = geoseries
-        return output
+        output.update(other_fields_to_output)
+        return gpd.GeoDataFrame(output)
